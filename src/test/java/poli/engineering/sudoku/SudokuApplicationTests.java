90d82e43d53161b08044ab71979627d565d0ab1b
SebastianLopezO <sebastianlopezosorno2005@gmail.com>
2025-08-14 19:17:37 -0500

    Initial commit

---
+++ src/test/java/poli/engineering/sudoku/SudokuApplicationTests.java
@@ -0,0 +1,13 @@
+package poli.engineering.sudoku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
